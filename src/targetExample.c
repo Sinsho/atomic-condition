d04079241cf46b50f87c9d1693b6d863404137de
Nicolas Kupper <nkupper@student.ethz.ch>
2022-05-24 11:54:43 +0200

    Update README.md

--- src/targetExample.c
+++ src/targetExample.c
@@ -1,11 +1,6 @@
 #include <math.h>
 
 double foo(double x, double y, double z) {
-    // This function calculate the motivation example:
-    // y = (1-cos(x))/(x^2)
-    // See details at: https://www.wolframalpha.com/input/?i=(1-cos(x))%2F(x%5E2)
-
-    // It triggers significant error when x->0, where y should be ->0.5.
     double m = x * y +z;
     double k = m * z;
     double val = (1-cos(x*y))/(x*x*k);
